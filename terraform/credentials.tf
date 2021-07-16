45e138dca2781a13551c3167b03f31126a3b326f
Heliotropo <afsanchezop@gmail.com>
2021-07-16 23:09:02 +0200

    Inicializar algunas variable

--- terraform/credentials.tf
+++ terraform/credentials.tf
@@ -2,8 +2,8 @@
 # crea un service principal y rellena los siguientes datos para autenticar
 provider "azurerm" {
   features {}
-  subscription_id = "<SUBSCRIPTION_ID>"
-  client_id       = "<APP_ID>"         # se obtiene al crear el service principal
-  client_secret   = "<CLIENT_SECRET>"  # se obtiene al crear el service principal
-  tenant_id       = "<TENANT_ID>"      # se obtiene al crear el service principal
+  subscription_id = "4ec2419b-8946-4ae3-afc5-29d158a3aa21"
+  client_id       = "c25e6c93-af59-44bd-acc9-5aa685dde9f4"         # se obtiene al crear el service principal
+  client_secret   = "jG5qRM4PyyiU3a.YQeSsq8j3yZnIe4gOZm"  # se obtiene al crear el service principal
+  tenant_id       = "899789dc-202f-44b4-8472-a6d40f9eb440"      # se obtiene al crear el service principal
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ provider "azurerm" {
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 
 resource "azurerm_resource_group" "rg" {
-    name     =  "kubernetes_rg"
+    name     =  "NetworkWatcherAFS"
     location = var.location
 
     tags = {
@@ -34,7 +34,7 @@ resource "azurerm_resource_group" "rg" {
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
 
 resource "azurerm_storage_account" "stAccount" {
-    name                     = "staccountcp2" 
+    name                     = var.storage_account 
     resource_group_name      = azurerm_resource_group.rg.name
     location                 = azurerm_resource_group.rg.location
     account_tier             = "Standard"
