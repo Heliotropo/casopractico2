2fa46f32f7c80e0258387b42175a07b9bf43f58d
Heliotropo <afsanchezop@gmail.com>
2021-07-17 13:09:51 +0200

    Agregar maquinas a la infraestructura

--- terraform/network.tf
+++ terraform/network.tf
@@ -60,3 +60,117 @@ resource "azurerm_public_ip" "myPublicIp1" {
     }
 
 }
+
+# Create NIC
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface
+
+resource "azurerm_network_interface" "myNic2" {
+  name                = "vmnic2"  
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+    name                           = "myipconfiguration2"
+    subnet_id                      = azurerm_subnet.mySubnet.id 
+    private_ip_address_allocation  = "Static"
+    private_ip_address             = "10.0.1.11"
+    public_ip_address_id           = azurerm_public_ip.myPublicIp2.id
+  }
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# IP pública
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip
+
+resource "azurerm_public_ip" "myPublicIp2" {
+  name                = "vmip2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# Create NIC
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface
+
+resource "azurerm_network_interface" "myNic3" {
+  name                = "vmnic3"  
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+    name                           = "myipconfiguration3"
+    subnet_id                      = azurerm_subnet.mySubnet.id 
+    private_ip_address_allocation  = "Static"
+    private_ip_address             = "10.0.1.12"
+    public_ip_address_id           = azurerm_public_ip.myPublicIp1.id
+  }
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# IP pública
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip
+
+resource "azurerm_public_ip" "myPublicIp3" {
+  name                = "vmip3"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# Create NIC
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface
+
+resource "azurerm_network_interface" "myNic4" {
+  name                = "vmnic4"  
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+    name                           = "myipconfiguration4"
+    subnet_id                      = azurerm_subnet.mySubnet.id 
+    private_ip_address_allocation  = "Static"
+    private_ip_address             = "10.0.1.20"
+    public_ip_address_id           = azurerm_public_ip.myPublicIp1.id
+  }
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# IP pública
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip
+
+resource "azurerm_public_ip" "myPublicIp4" {
+  name                = "vmip4"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
