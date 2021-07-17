1d9865a7929197997e9ec4da1be1ae3648cbf53b
Heliotropo <afsanchezop@gmail.com>
2021-07-17 12:51:57 +0200

    Cambio variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ terraform {
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 
 resource "azurerm_resource_group" "rg" {
-    name     =  "NetworkWatcherAFS"
+    name     =  "CasoPractico2RG"
     location = var.location
 
     tags = {
--- terraform/vm.tf
+++ terraform/vm.tf
@@ -2,16 +2,16 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
 
 resource "azurerm_linux_virtual_machine" "myVM1" {
-    name                = "my-first-azure-vm"
+    name                = "Master"
     resource_group_name = azurerm_resource_group.rg.name
     location            = azurerm_resource_group.rg.location
     size                = var.vm_size
-    admin_username      = "adminUsername"
+    admin_username      = "Antonio"
     network_interface_ids = [ azurerm_network_interface.myNic1.id ]
     disable_password_authentication = true
 
     admin_ssh_key {
-        username   = "adminUsername"
+        username   = "Antonio"
         public_key = file("~/.ssh/id_rsa.pub")
     }
 
