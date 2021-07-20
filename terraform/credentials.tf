9829221bcc026821f92a52f601c4f3daec603e64
Heliotropo <afsanchezop@gmail.com>
2021-07-20 18:01:14 +0200

    Corregida vulnerabilidad: avisado por git guarden

--- terraform/credentials.tf
+++ terraform/credentials.tf
@@ -5,6 +5,6 @@ provider "azurerm" {
   features {}
   subscription_id = "4ec2419b-8946-4ae3-afc5-29d158a3aa21"
   client_id       = "c25e6c93-af59-44bd-acc9-5aa685dde9f4"         # se obtiene al crear el service principal
-  client_secret   = "jG5qRM4PyyiU3a.YQeSsq8j3yZnIe4gOZm"  # se obtiene al crear el service principal
+  client_secret   = "***"  # se obtiene al crear el service principal
   tenant_id       = "899789dc-202f-44b4-8472-a6d40f9eb440"      # se obtiene al crear el service principal
 }
